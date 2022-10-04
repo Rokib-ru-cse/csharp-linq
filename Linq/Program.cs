f9c0678da71e9c22b7462422b99d1b790480dc87
rokibbizz <111946001+rokibbizz@users.noreply.github.com>
2022-10-04 15:39:00 +0600

    linq added

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -2,9 +2,50 @@
 
 
 using Linq;
+using System.Collections.Generic;
 
 var comments = Comment.GetComments();
 var posts = Post.GetPosts();
 var todos = Todo.GetTodos();
 var users = User.GetUsers();
+
+var filteredUser = users.Select(u => new /*User*/
+{
+    id = u.id,
+    name = u.name,
+    email = u.email,
+    phone = u.phone,
+    username = u.username,
+    website = u.website,
+    company = u.company,
+    address = u.address,
+    todos = todos.Where(t=>t.userId==u.id).ToList(),
+    posts = posts.Where(p=>p.userId)
+
+}).ToList();
+
+var selectedPosts = posts.Select(p => new {
+    id=p.id,
+    comments = comments.Where(c => c.postId == p.id).ToList(),
+} ).ToList();
+
+var selectedComments = (from u in users
+                        join t in todos 
+                        on u.id equals t.userId
+                       join p in posts
+                       on u.id equals p.userId
+                       join c in comments
+                       on p.id equals c.postId
+                        select new
+                        {
+                            id = u.id,
+                            todos =t,
+                            posts = p,
+
+                        }).ToList();
+
+
+
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
